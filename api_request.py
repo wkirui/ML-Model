75c6e374e6965dad001167c2b122189ecbb9299f
Wesley Kirui <kkiruiwesley@gmail.com>
2020-12-18 00:11:40 +0300

    updated api values

--- api_request.py
+++ api_request.py
@@ -2,11 +2,11 @@ import requests
 url = 'http://localhost:5000/resultsApi'
 
 r =  requests.post(url, 
-                   json={'guests':1,'bedrooms':'1',
+                   json={'guests':2,'bedrooms':'1',
                          'beds':'1','bathrooms':'1',
-                         'total_reviews':12,'review_score':4,
+                         'total_reviews':12,'rating':4,
                          'kitchen':'yes','is_superhost':'yes',
-                         'wifi':'no','parking':'yes',
+                         'wifi':'yes','parking':'yes',
                          'pool':'no','shared_bath':'no'}
                    )
 
