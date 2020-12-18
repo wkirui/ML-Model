6c25f9631b98f82c7cf8a83fb7336e813a6d0d53
Wesley Kirui <kkiruiwesley@gmail.com>
2020-12-18 15:20:48 +0300

    minor edit

--- api_request.py
+++ api_request.py
@@ -1,10 +1,11 @@
 import requests
-url = 'http://localhost:8000/resultsApi'
+# url = 'http://localhost:8000/resultsApi'
+url = 'https://naimodel.herokuapp.com/resultsApi'
 
 r =  requests.post(url, 
-                   json={'guests': 3,'bedrooms': 2,
+                   json={'guests': 4,'bedrooms': 2,
                          'beds': 2,'baths': 1,
-                         'reviews':135,'rating': 4,
+                         'reviews':45,'rating': 4,
                          'kitchen':'yes','is_superhost':'no',
                          'wifi':'yes','parking':'yes',
                          'pool':'no','shared_bath':'yes'}
