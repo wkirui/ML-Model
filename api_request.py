6ffa0ba42e6336b521dcd6a6d753f2ad342b4d48
Wesley Kirui <kkiruiwesley@gmail.com>
2020-12-18 22:34:01 +0300

    minor edit

--- api_request.py
+++ api_request.py
@@ -1,6 +1,6 @@
 import requests
-# url = 'http://localhost:8000/resultsApi'
-url = 'https://naimodel.herokuapp.com/resultsApi'
+url = 'http://127.0.0.1:5000/resultsApi'
+# url = 'https://naimodel.herokuapp.com/resultsApi'
 
 r =  requests.post(url, 
                    json={'guests': 4,'bedrooms': 2,
