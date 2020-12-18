1b07d57d98fe9f8afed229250efeae3679d54e58
Wesley Kirui <kkiruiwesley@gmail.com>
2020-12-18 14:40:46 +0300

    updated api values

--- api_request.py
+++ api_request.py
@@ -1,13 +1,13 @@
 import requests
-url = 'http://localhost:5000/resultsApi'
+url = 'http://localhost:8000/resultsApi'
 
 r =  requests.post(url, 
-                   json={'guests':2,'bedrooms':1,
-                         'beds':1,'baths':1,
-                         'reviews':12,'rating':4,
-                         'kitchen':'yes','is_superhost':'yes',
+                   json={'guests': 3,'bedrooms': 2,
+                         'beds': 2,'baths': 1,
+                         'reviews':135,'rating': 4,
+                         'kitchen':'yes','is_superhost':'no',
                          'wifi':'yes','parking':'yes',
-                         'pool':'no','shared_bath':'no'}
+                         'pool':'no','shared_bath':'yes'}
                    )
 
 print(r.json())
